f2cdc7f3b4f9dcee2a4c65acc6c16cc5caa76144
vnashBio <46813734+vnashBio@users.noreply.github.com>
2020-08-23 02:58:48 +0200

    first commit

---
+++ client/src/components/greetings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class greetings extends Component {
+    state = {}
+    render() {
+        return (
+            <div><h1>welcome to chat room</h1></div>
+        );
+    }
+}
+
